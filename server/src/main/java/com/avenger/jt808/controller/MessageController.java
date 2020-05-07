7378548632545fdec799e28e2cf83b312d127836
jungang wang <jgwang03@gmail.com>
2020-05-07 10:38:17 +0800

    feat: sse服务端推送消息结果

--- server/src/main/java/com/avenger/jt808/controller/MessageController.java
+++ server/src/main/java/com/avenger/jt808/controller/MessageController.java
@@ -6,16 +6,18 @@ import com.avenger.jt808.domain.Header;
 import com.avenger.jt808.domain.Message;
 import com.avenger.jt808.server.TermConnManager;
 import com.avenger.jt808.service.MessageRecordService;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
 
 /**
  * Created by jg.wang on 2020/4/30.
@@ -30,7 +32,7 @@ public class MessageController {
     @NonNull
     private final MessageRecordService messageRecordService;
 
-    @PostMapping("shoot")
+    @PostMapping(value = "shoot", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 //    @ApiOperation(value = "立即拍摄")
     public Flux<?> send(@RequestBody Shoot shoot) {
         Header header = new Header(shoot.getSimNo(), false, EncryptionForm.NOTHING);
--- server/src/main/java/com/avenger/jt808/service/impl/MessageRecordServiceImpl.java
+++ server/src/main/java/com/avenger/jt808/service/impl/MessageRecordServiceImpl.java
@@ -2,6 +2,7 @@ package com.avenger.jt808.service.impl;
 
 import com.avenger.jt808.domain.entity.MessageRecord;
 import com.avenger.jt808.domain.repository.MessageRecordRepository;
+import com.avenger.jt808.enums.MessageFlow;
 import com.avenger.jt808.enums.MessageRecordStatus;
 import com.avenger.jt808.service.MessageRecordService;
 import lombok.NonNull;
@@ -20,6 +21,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by jg.wang on 2020/5/6.
@@ -45,7 +47,12 @@ public class MessageRecordServiceImpl implements MessageRecordService {
      */
     @Override
     public Flux<?> sendCheck(String simNo, short serialNo) {
-        return Flux.interval(Duration.ofMillis(200)).zipWith(Flux.create(fluxSink -> {
+        final AtomicInteger atomicInteger = new AtomicInteger(0);
+        return Flux.interval(Duration.ofMillis(20)).zipWith(Flux.generate(fluxSink -> {
+            if (atomicInteger.incrementAndGet() < 2) {
+                fluxSink.next(Collections.singletonMap("message", "发送中"));
+                return;
+            }
             final Optional<MessageRecord> one = messageRecordRepository.findOne((Specification<MessageRecord>) (root, query, criteriaBuilder) -> {
                 final LocalDateTime now = LocalDateTime.now();
 
@@ -53,15 +60,17 @@ public class MessageRecordServiceImpl implements MessageRecordService {
                 predicates.add(criteriaBuilder.equal(root.get("simNo"), simNo));
                 predicates.add(criteriaBuilder.equal(root.get("serialNo"), serialNo));
                 predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("createdAt"), now));
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("flowTo"), MessageFlow.SEND));
                 predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("createdAt"), now.minusMinutes(20)));
                 return query
                         .where(predicates.toArray(new Predicate[0])).getRestriction();
             });
             one.ifPresent(messageRecord -> {
                 if (messageRecord.getStatus() == MessageRecordStatus.RESPONDED) {
+                    fluxSink.next(Collections.singletonMap("message", "终端已回复"));
                     fluxSink.complete();
                 } else {
-                    fluxSink.next(Collections.singletonMap("message", "发送中"));
+                    fluxSink.next(Collections.singletonMap("message", "已发送，等待终端回复"));
                 }
             });
             if (!one.isPresent()) {
